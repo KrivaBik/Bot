5c3c5ffa338b1d267ec955c448334f31beff16e4
Evgen <Akei@i.ua>
2018-02-25 23:02:12 +0200

    13

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ var CronJob = require('cron').CronJob;
 const TOKEN = '526588233:AAHhtkEj3Jq6A7pP048DItlam9k03IR99vA';
 
 
-
-
-
 try {
     var appConfig=require ('./appConfig');
     var configFileName=process.argv[2]|| "configPro";
@@ -24,14 +21,7 @@ var appPort= appConfig.getAppConfigParam("appPort") || 80;
 var Schedule= appConfig.getAppConfigParam("Schedule");
 
 try{
-    let dataMeal=JSON.parse(fs.readFileSync(__dirname+'/dataMeal.json'));
-    if(!dataMeal||dataMeal.length==0) return;
-
-    for (var i=0;i<dataMeal.length;i++){
-        var dataMealI=dataMeal[i];
-        startScheduleMeals(dataMealI);
-        console.log(i);
-    }
+    sendMealsToUser();
 
 }catch (e){
     console.log("Nooooo",e.message);
@@ -43,26 +33,66 @@ try{
 const bot =new TelegramBot(TOKEN, {
     polling:true
 });
-var KB={
+let KB={
     menu:'Меню',
     chooseDietOptions:'Выбор диеты',
     countCal:'Подсчет дневной нормы калорий',
+    calcCal:'Расчет нормы для меня'
+
+};
+let InlKB={
     losingWeight:'LosingWeight',
     muscleGain:'MuscleGain',
-    backToMenu:'BackToMenu'
-};
-// const PicScrs={
-//   [KB.cat]:[
-//       'cat1.jpg',
-//       'cat2.jpg',
-//       'cat3.jpg'
-//   ],
-//     [KB.car]:[
-//         'brf1.jpg'
-//     ]
-// };
-// var registeredUsers=JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
+    backToMenu:'BackToMenu',
+    countCalMuscleGain:'CountCalMuscleGain',
+    countCalLosingWeight:'CountCalLosingWeight'
 
+};
+bot.onText(/Возраст=/,function (msg) {
+    let age=parseInt(msg.text.replace(/\D+/g,""));
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    if(userParam[msg.chat.id]){
+        userParam[msg.chat.id].age=age;
+    }
+    fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+    bot.sendMessage(msg.chat.id,'Введите ваш вес ввиде "Вес=Х", где X - округленный вес в килограммах.')
+});
+
+bot.onText(/Вес=/,function (msg) {
+    let weight=parseInt(msg.text.replace(/\D+/g,""));
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    if(userParam[msg.chat.id]){
+        userParam[msg.chat.id].weight=weight;
+    }
+    fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+    bot.sendMessage(msg.chat.id,'Введите ваш рост ввиде  "Рост=Х", где X - округленный рост в сантиметрах.')
+});
+bot.onText(/Рост=/,function (msg) {
+    let growth=parseInt(msg.text.replace(/\D+/g,""));
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    if(userParam[msg.chat.id]){
+        userParam[msg.chat.id].growth=growth;
+    }
+    fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+    bot.sendMessage(msg.chat.id,'Введите ваш пол ввиде  "Пол=Х", где X - 1 мужчина,Х - 0 женщина.')
+
+});
+bot.onText(/Пол=/,function (msg) {
+    let sex=parseInt(msg.text.replace(/\D+/g,""));
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    if(userParam[msg.chat.id]){
+        userParam[msg.chat.id].sex=sex;
+    }
+    fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+    bot.sendMessage(msg.chat.id,':)',{
+        reply_markup:{
+            keyboard:[
+                [KB.calcCal]
+            ]
+        }
+    })
+
+});
 bot.onText(/\/start/, msg => {
     let registeredUsers=JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
     let mesg, objKB;
@@ -85,11 +115,20 @@ bot.onText(/\/start/, msg => {
 });
 bot.on('contact',msg=>{
     toMenu(msg.chat.id);
+
     let registeredUsers=JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
     if(!registeredUsers[msg.chat.id]){
         registeredUsers[msg.chat.id]={'phone':msg.contact.phone_number,'diet':'' };
+
     }
     fs.writeFileSync((__dirname+'/idBase.json'),JSON.stringify(registeredUsers));
+
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    if(!userParam[msg.chat.id]){
+        userParam[msg.chat.id]={'weight':'','growth':'','sex':'','target':''};
+    }
+    fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+    sendMealsToUser();
 });
 
 bot.on('message', msg=>{
@@ -103,33 +142,8 @@ bot.on('message', msg=>{
         case KB.countCal:
             sendCurrencyScreen(msg.chat.id);
             break;
-        // case KB.car:
-        // case KB.cat:
-        //     sendPictureByName(msg.chat.id, msg.text);
-        //     break;
-        // case KB.back:
-        //     sendgreeting2(msg, false);
-        //     break;
-
-
-    }
-
-});
-bot.on('message', msg=>{
-    var listUsers =JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
-    var chatId=msg.chat.id;
-    switch (msg.text){
-        case KB.losingWeight:
-            listUsers.chatId.diet=KB.losingWeight;
-            fs.writeFile((__dirname+'/idBase.json'),JSON.stringify(listUsers),function () {
-                bot.sendMessage(chatId,"Вы успешно зарегистрированы, ожидайте сообщение с рецептом)");
-            });
-            break;
-        case KB.muscleGain:
-            listUsers.chatId.diet=KB.losingWeight;
-            fs.writeFile((__dirname+'/idBase.json'),JSON.stringify(registeredUsers),function () {
-                bot.sendMessage(chatId,"Вы успешно зарегистрированы, ожидайте сообщение с рецептом)");
-            });
+        case KB.calcCal:
+            resultCalcCal(msg.chat.id);
             break;
     }
 
@@ -164,29 +178,53 @@ bot.on('message', msg=>{
 
 bot.on('callback_query', query=>{
     switch (query.data){
-        case KB.backToMenu:
+        case InlKB.backToMenu:
             toMenu(query.message.chat.id);
             bot.answerCallbackQuery({
                 callback_query_id:query.id,
                 text:'Меню'
             });
             break;
-        case KB.losingWeight:
-            addDietToIdBase(query.message.chat.id,KB.losingWeight);
+        case InlKB.losingWeight:
+            addDietToIdBase(query.message.chat.id,InlKB.losingWeight);
             bot.answerCallbackQuery({
                 callback_query_id:query.id,
                 text:'Ожидайте низкокалорийные рецепты'
             });
             break;
-        case KB.muscleGain:
-            addDietToIdBase(query.message.chat.id,KB.muscleGain);
+        case InlKB.muscleGain:
+            addDietToIdBase(query.message.chat.id,InlKB.muscleGain);
             bot.answerCallbackQuery({
                 callback_query_id:query.id,
                 text:'Ожидайте высокопитательные рецепты'
             });
             break;
+        case InlKB.countCalMuscleGain:
+            addTargetToIdBase(query.message.chat.id,InlKB.countCalMuscleGain);
+            bot.answerCallbackQuery({
+                callback_query_id:query.id,
+                text:'Следуйте дальнейшим инструкциям'
+            });
+            break;
+        case InlKB.countCalLosingWeight:
+            addTargetToIdBase(query.message.chat.id,InlKB.countCalLosingWeight);
+            bot.answerCallbackQuery({
+                callback_query_id:query.id,
+                text:'Следуйте дальнейшим инструкциям'
+            });
+            break;
     }
 });
+function sendMealsToUser() {
+    let dataMeal=JSON.parse(fs.readFileSync(__dirname+'/dataMeal.json'));
+    if(!dataMeal||dataMeal.length==0) return;
+
+    for (var i=0;i<dataMeal.length;i++){
+        var dataMealI=dataMeal[i];
+        startScheduleMeals(dataMealI);
+    }
+
+}
 function toMenu(Id) {
     let objKB ={reply_markup: {
         keyboard: [
@@ -206,21 +244,20 @@ function sendMenu(chatId) {
     });
 }
 function chooseDietOptions(Id) {
-
     bot.sendMessage(Id,'Выбор диеты', {
         reply_markup:{
             inline_keyboard:[
                 [{
                     text:'Жиросжигание',
-                    callback_data:KB.losingWeight
+                    callback_data:InlKB.losingWeight
                 }],
                 [{
                     text:'Набор мышечной массы',
-                    callback_data:KB.muscleGain
+                    callback_data:InlKB.muscleGain
                 }],
                 [{
                     text:'Назад к меню',
-                    callback_data:KB.backToMenu
+                    callback_data:InlKB.backToMenu
                 }]
             ]
         }
@@ -230,50 +267,51 @@ function addDietToIdBase(Id,Value) {
     let listUsers =JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
     let chatId=Id;
     switch (Value){
-        case KB.losingWeight:
-            listUsers[chatId].diet=KB.losingWeight;
+        case InlKB.losingWeight:
+            listUsers[chatId].diet=InlKB.losingWeight;
             fs.writeFile((__dirname+'/idBase.json'),JSON.stringify(listUsers));
             break;
-        case KB.muscleGain:
-            listUsers[chatId].diet=KB.muscleGain;
+        case InlKB.muscleGain:
+            listUsers[chatId].diet=InlKB.muscleGain;
             fs.writeFile((__dirname+'/idBase.json'),JSON.stringify(listUsers));
             break;
     }
 }
 
+function addTargetToIdBase(Id,Value) {
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    let chatId=Id;
+    switch (Value){
+        case InlKB.countCalLosingWeight:
+            userParam[chatId].target=InlKB.countCalLosingWeight;
+            fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+            break;
+        case InlKB.countCalMuscleGain:
+            userParam[chatId].target=InlKB.countCalMuscleGain;
+            fs.writeFileSync((__dirname+'/userParams.json'),JSON.stringify(userParam));
+            break;
+    }
+    bot.sendMessage(Id,'Введите ваш возраст ввиде "Возраст=Х", где X - округленный возраст .')
+}
+
 function startScheduleMeals(dataMeal){
-    var listUsers =JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
+    let listUsers =JSON.parse(fs.readFileSync(__dirname+'/idBase.json'));
     let job=new CronJob(dataMeal.schedule,
         function() {
-            for (k in listUsers){
-                var rand = Math.floor(Math.random() * dataMeal.meals.length);
+            for (let k in listUsers){
+                let rand = Math.floor(Math.random() * dataMeal.meals.length);
                 // bot.sendMessage(k,dataMeal.meals[rand]);
-
-                var src=dataMeal.meals[rand];
+                let src=dataMeal.meals[rand];
                 bot.sendMessage(k,dataMeal.welcome);
-                fs.readFile(__dirname+'/pictures/'+src,(err,picture)=>{
-                    if (err) throw new Error(err);
-                    bot.sendPhoto(k,picture).then(()=>{
-                        bot.sendMessage(k,"Приятного апетита");
-                    });
-                })
+                let picture=fs.readFileSync(__dirname+'/pictures/'+src);
+                bot.sendPhoto(k, picture).then(() => {
+                    bot.sendMessage(k, "Приятного апетита");
+                });
+
             }
         }
         , null, true, 'Europe/Kiev');
 }
-
-
- // function sendPictureScreen(chatId) {
- //     bot.sendMessage(chatId,'viberi tip pecture', {
- //         reply_markup:{
- //             keyboard:[
- //                 [KB.car,KB.cat],
- //                 [KB.back]
- //             ]
- //         }
- //     })
- // }
-
 // function sendgreeting2(msg) {
 //
 //     const text= sendHello
@@ -302,19 +340,38 @@ function startScheduleMeals(dataMeal){
 // }
 
 function sendCurrencyScreen(chatId) {
-     bot.sendMessage(chatId, 'viberi kurs valut',{
+     bot.sendMessage(chatId, 'Выберете вашу цель',{
          reply_markup:{
              inline_keyboard:[
                  [{
                  text:'Рост мышц',
-                     callback_data:'MuscleGain'
+                     callback_data:InlKB.countCalMuscleGain
                  }],
                  [{
                      text:'Сжигание жира',
-                     callback_data:'LosingWeight'
+                     callback_data:InlKB.countCalLosingWeight
                  }]
              ]
          }
-     })
-    console.log(query.data);
+     });
 }
+function resultCalcCal(Id) {
+    let userParam=JSON.parse(fs.readFileSync(__dirname+'/userParams.json'));
+    let userParamId=userParam[Id];
+    let cal;
+    if(userParamId){
+        if (userParamId.sex==1){
+            cal=88.36+(13.4*userParamId.weight)+(4.8*userParamId.growth)-(5.7 *userParamId.age)
+        }else {
+            cal=447.6+(9.2 *userParamId.weight)+(3.1*userParamId.growth)-(4.3 *userParamId.age)
+        }
+        bot.sendMessage(Id, 'Ваша норма='+cal,{
+            reply_markup:{
+                keyboard:[
+                    [KB.menu]
+                ]
+            }
+        });
+    }
+}
+
